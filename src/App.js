0e3107e87a08b0fa939512a105144c76a5170f58
volta2016 <giovoltadesign@gmail.com>
2021-11-24 11:58:27 -0300

    [jsx] definir components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const names = ['Kyo', 'Maxi', 'Afrodita', 'Agus'];
-
-/*function getNames() {
-  const elementsList = [];
-  
-  for (let i = 0; i < names.length; i++) elementsList.push(<li key={names[i]}>{names[i]}</li>);
-  
-  return elementsList;
-}*/
-
-const Names = () => {
-  function generateId() {
-    return Math.random() * 1;
-  }
-
-  return (
-    <ul>
-      {names.map((name) => (
-        <li key={generateId()}>{name}</li>
-      ))}
-    </ul>
-  );
-};
-
-const App = () => {
-  return (
-    <div>
-      <Names />
-    </div>
-  );
-};
+const App = () => <h1>Hola</h1>;
 
 export default App;
 
