8244ff58febb7ca9d18a6129d9c954830c31f5b8
volta2016 <giovoltadesign@gmail.com>
2021-11-28 01:02:28 -0300

    [jsx] class Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-import Form from "./components/Form";
+import PropTypes from "proptypes";
 
-export const ThemeContext = React.createContext();
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+    };
+  }
 
-const App = () => {
-  return (
-    <>
-      <Form />
-    </>
-  );
-};
+  static defaultProps = {
+    name: "Cody Default",
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <>
+        <p>Contador: {this.state.contador}</p>
+        <button
+          onClick={() => this.setState({ contador: this.state.contador + 1 })}
+        >
+          Add
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
 
